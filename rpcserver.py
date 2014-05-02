b3b761b7cebf068a553349f5dee1c04e3cff65dc
ahmedbodi <ahmed_bodi@msn.com>
2014-05-02 13:33:53 +0100

    Update rpcserver.py

--- rpcserver.py
+++ rpcserver.py
@@ -11,15 +11,40 @@ config.verbose = True
 
 def change_coin(address, code, uri, p2p_port, network_id, upstream_port):
 	try:
-		settings.TracketAddr = address
-		settings.name = code
-		settings.uri = uri
-		settings.UpstreamBitcoinNode = ('127.0.0.1', p2p_port)
-		settings.UpstreamNetworkId = b + str(network_id)
-		settings.BitcoinNodeAddresses = ('', upstream_port)
+		f.open(config.py, w)
+		f.write("Server Name = 'CryptoPools.com'")
+		f.write("ShareTarget = 0x0000ffff00000000000000000000000000000000000000000000000000000000")
+		f.write("DynamicTargetting = 1")
+		f.write("DynamicTargetGoal = 10")
+		f.write("DynamicTargetWindow = 300")
+		f.write("WorkQueueSizeRegular = (0x100, 0x1000)")
+		f.write("WorkQueueSizeClear = (0x1000, 0x2000)")
+		f.write("WorkQueueSizeLongpoll = (0x1000, 0x2000)")
+		f.write("MinimumTxnUpdateWait = 5")
+		f.write("TxnUpdateRetryWait = 1")
+		f.write("IdleSleepTime = 0.1")
+		f.write("TrackerAddr = {}".format(address))
+		f.write("UpstreamURI = {}".format(uri))
+		f.write("DelayLogForUpstream = True")
+		f.write("UpstreamBitcoindNode = ('127.0.0.1', {})".format(p2p_port))
+		f.write("UpstreamNetworkId = b'{}'".format(network_id))
+		f.write("SecretUser = ''")
+		f.write("GotWorkURI = ''")
+		f.write("GotWorkTarget = 0x0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")
+		f.write("POT = 0")
+		f.write("Greedy = False")
+		f.write("JSONRPCAddresses = (('::ffff:176.119.27.170', 8344),)")
+		f.write("StratumAddresses = (('::ffff:176.119.27.170', 3333),)")
+		f.write("BitcoinNodeAddresses = (('::ffff:176.119.27.170', {}),)".format(upstream_port))
+		f.write("TrustedForwarders = ('::ffff:127.0.0.1',)")
+		f.write("ShareLogging = ({'type': 'logfile', 'filename': 'share-logfile', 'format': "{time} {Q(remoteHost)} {username} {YN(not(rejectReason))} {dash(YN(upstreamResult))} {dash(rejectReason)} {solution}\n),)")
+		f.write("Authentication =  ({'module': 'allowall',},)")
+		f.write("LogFile = 'filename.log')
+		f.close()
 		return True
 	except:
 		return False
 		
-rpcsrv = Server((eloipool_config.rpchost:eloipool_config.rpcport))
+rpcsrv = Server(('127.0.0.1':9001))
 rpcsrv.add_handler(change_coin)
+rpcsrv.serve_forever()
